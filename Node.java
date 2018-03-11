a41a7a82f23414185c578731e3d86a5a13cef070
Darryl Pinto <dp6417@rit.edu>
2018-03-11 17:04:33 -0400

    Server programming with threads

--- Node.java
+++ Node.java
@@ -1,5 +1,32 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by Darryl Pinto on 3/11/2018.
  */
 public class Node {
+
+    public static void main(String[] args) throws IOException {
+
+        String host = "LocalHost";
+        Scanner sc = new Scanner(System.in);
+        Socket soc = new Socket(host, 6000);
+
+        // 1
+        ObjectOutputStream output = new ObjectOutputStream(soc.getOutputStream());
+        ObjectInputStream input = new ObjectInputStream(soc.getInputStream());
+
+        System.out.println("Enter username: ");
+        String username = sc.next();
+        output.writeUTF(username);
+        output.flush();
+
+
+
+
+
+    }
 }
--- RegistrationServer.java
+++ RegistrationServer.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 
 /**
@@ -5,7 +10,44 @@ import java.util.HashMap;
  */
 public class RegistrationServer {
     static final private int N = 16;
-    HashMap<String,>
+    static final private int serverPort = 6000;
+
+    HashMap<String, Socket> connectionMap = new HashMap<String, Socket>();
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSock = new ServerSocket(6000);
+
+        while(true) {
+            Socket soc = serverSock.accept();
+            ServerOperation sop = new ServerOperation(soc);
+            new Thread(sop).start();
+            //1
+
+        }
+    }
 
 }
 
+class ServerOperation implements Runnable{
+
+    Socket socket;
+
+    ServerOperation(Socket socket){
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+
+        try {
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+            String abc = input.readUTF();
+            System.out.println(abc);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
